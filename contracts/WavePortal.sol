3ecd60ede9a5e43e3aa8ebb205d156d6795d45e3
Paulo Hernane <paulohernane10@gmail.com>
2022-06-11 18:32:33 -0300

    create smart contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    constructor() {
+        console.log("Este e um contrato inteligente");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s deu tchauzinho!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("Temos um total de %d tchauzinhos!", totalWaves);
+        return totalWaves;
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,27 @@
+import * as hre from "hardhat";
+
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account Balance: ", accountBalance.toString());
+
+  const Token = await hre.ethers.getContractFactory("WavePortal");
+  const portal = await Token.deploy();
+  await portal.deployed();
+
+  console.log("WaveProtal address: ", portal.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
